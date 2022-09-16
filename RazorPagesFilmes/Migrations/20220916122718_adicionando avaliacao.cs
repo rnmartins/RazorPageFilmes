6e3936351897fe007f2c814b1c06b046f5a67d51
renan <renansmartins@live.com>
2022-09-16 10:45:21 -0300

    Adicionar arquivos de projeto.

---
+++ RazorPagesFilmes/Migrations/20220916122718_adicionando avaliacao.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPagesFilmes.Migrations
+{
+    public partial class adicionandoavaliacao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Avaliacao",
+                table: "Filme",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Avaliacao",
+                table: "Filme");
+        }
+    }
+}
---
+++ RazorPagesFilmes/Model/Filme.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesFilmes.Model
+{
+    public class Filme
+    {
+        public int ID { get; set; }
+
+        [StringLength(100, MinimumLength = 3)]
+        [Display(Name = "Título")]
+        public string Titulo { get; set; } = string.Empty;
+
+        [Display(Name = "Data de Lançamento")]
+        [DataType(DataType.Date)]
+        public DateTime DataLancamento { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        [Display(Name = "Gênero")]
+        public string Genero { get; set; } = string.Empty;
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
+        [Display(Name = "Preço")]
+        public decimal Preco { get; set; }
+
+        [Display(Name = "Avaliação")]
+        [Range(0, 5)]
+        public int Avaliacao { get; set; } = 0;
+    }
+}
